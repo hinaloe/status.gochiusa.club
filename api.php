d2d7a30a46f4377cd6b73bbd9b4a2b0d29b60dc1
gaogao-9 <gaotuki@gmail.com>
2016-01-17 02:24:21 +0900

    コードリファクタリング各種

--- api.php
+++ api.php
@@ -8,129 +8,127 @@ $time_start = microtime(true);
 
 include "getMultiCotents.php";
 
-// メンバーのリスト　並び順は http://www.gochiusa.com/contents/chara/index.html に準拠。
+// メンバーのリスト 並び順は http://www.gochiusa.com/contents/chara/index.html に準拠。
 $member_list = array(
-    "64319102",   // ココア @otack
-    "538308036",  // チノ　 @yaplus
-    "14157941",   // リゼ　 @karno
-    "873775722",  // 千夜　 @eai04191
-    "2439755767", // シャロ @akouryy1
-    "2239375134", // マヤ　 @aayh
-    "547406123",  // メグ　 @su2ca
-    "463401611",  // モカ　 @kazukiti_28
+    "64319102"   => array(
+        "chara" => "cocoa",
+        "names" => array("心愛","ココア"), // @otack
+    ),
+    "538308036"  => array(
+        "chara" => "chino",
+        "names" => array("智乃","チノ"),   // @yaplus
+    ),
+    "14157941"   => array(
+        "chara" => "rize",
+        "names" => array("理世","リゼ"),   // @karno
+    ),
+    "873775722"  => array(
+        "chara" => "chiya",
+        "names" => array("千夜"),          // @eai04191
+    ),
+    "2439755767" => array(
+        "chara" => "syaro",
+        "names" => array("紗路","シャロ"), // @akouryy1
+    ),
+    "2239375134" => array(
+        "chara" => "maya",
+        "names" => array("麻耶","マヤ"),   // @aayh
+    ),
+    "547406123"  => array(
+        "chara" => "megumi",
+        "names" => array("恵","メグ"),     // @su2ca
+    ),
+    "463401611"  => array(
+        "chara" => "mocha",
+        "names" => array("モカ"),          // @kazukiti_28
+    ),
 );
 
-$member_count = count($member_list);
+// 何名居るかをキャッシュする
+$member_count = count( $member_list );
 
 // URL作成
-foreach( $member_list as $member_id ){
-  $member_url_list[] = "http://lab.mizle.net/status.gochiusa/user_information.php?id=".$member_id;
+$currentURL = (empty( $_SERVER["HTTPS"] ) ? "http" : "https")."://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
+foreach( $member_list as $id => $chara ){
+    $member_url_list[] = str_replace( "api.php", "", $currentURL ) . "user_information.php?id=".$id;
 }
 
 // 一気に取得（すごい早い）
-$results = getMultiContents( $member_url_list );
+$user_info_list = getMultiContents( $member_url_list );
 
 // 配列の頭を数字にして扱いやすく
-$results = array_values( $results );
-
-// 正しく読み込まれているか
-for ($count = 0; $count < $member_count; $count++){
-  if( $results[$count]["http_code"] == "0" ){
-    $result = array(
-        "status" => "error",
-        "data" => null,
-        "message" => "ユーザー情報の読み込みに失敗しました。"
-    );
-    die( json_encode( $result , JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES ) );
-  }
-}
-
-// JSON配列型でデコード
-$member_cocoa = json_decode( $results[0]["content"], true );
-$member_chino = json_decode( $results[1]["content"], true );
-$member_rize = json_decode( $results[2]["content"], true );
-$member_chiya = json_decode( $results[3]["content"], true );
-$member_syaro = json_decode( $results[4]["content"], true );
-$member_maya = json_decode( $results[5]["content"], true );
-$member_megu = json_decode( $results[6]["content"], true );
-$member_mocha = json_decode( $results[7]["content"], true );
-
+$user_info_list = array_values( $user_info_list );
 
 // ジャッジ
 $hopping_count = 0;
 
-if( strpos( $member_cocoa["name"], "心愛") !== false or strpos( $member_cocoa["name"], "ココア" !== false) ){
-    $member_cocoa["is_hopping"] = true;
-    $hopping_count++;
-}else{
-    $member_cocoa["is_hopping"] = false;
-}
+// 最終的なメンバー情報を入れるリスト
+$member_info_list = array();
 
-if( strpos( $member_chino["name"], "智乃") !== false or strpos( $member_chino["name"], "チノ" !== false) ){
-    $member_chino["is_hopping"] = true;
-    $hopping_count++;
-}else{
-    $member_chino["is_hopping"] = false;
+foreach( $user_info_list as $user_info ){
+    // 正しく読み込まれているか
+    if( $user_info["http_code"] !== 200 ){
+        $result = array(
+            "status" => "error",
+            "data" => null,
+            "message" => "ユーザー情報の読み込みに失敗しました。",
+        );
+        die( json_encode( $result , JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES ) );
+    }
+    
+    // JSON配列型でデコード
+    $member_info = @json_decode( $user_info["content"], true );
+    
+    // JSONの解釈に成功したか
+    if( ($member_info === null) && (json_last_error() !== JSON_ERROR_NONE) ){
+        $result = array(
+            "status" => "error",
+            "data" => null,
+            "message" => "ユーザー情報の読み込みに失敗しました。",
+        );
+        die( json_encode( $result , JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES ) );
+    }
+    
+    // 何らかのエラーメッセージを吐いたか
+    if( isset( $member_info["message"] ) ){
+        $result = array(
+            "status" => "error",
+            "data" => null,
+            "message" => "ユーザー情報の読み込みに失敗しました。(".$member_info["message"].")",
+        );
+        die( json_encode( $result , JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES ) );
+    }
+    
+    // 名前の一部分がごちうさキャラの名前かどうかを確認する
+    $member = $member_list[$member_info["id"]];
+    $member_info["is_hopping"] = false;
+    foreach( $member["names"] as $name ){
+        if( strpos($member_info["name"], $name) !== false ){
+            $member_info["is_hopping"] = true;
+            $hopping_count++;
+            break;
+        }
+    }
+    
+    // メンバー情報を入れるリストに追加する
+    $member_info_list[$member["chara"]] = $member_info;
 }
-
-if( strpos( $member_rize["name"], "理世") !== false or strpos( $member_rize["name"], "リゼ" !== false) ){
-    $member_rize["is_hopping"] = true;
-    $hopping_count++;
-}else{
-    $member_rize["is_hopping"] = false;
-}
-
-if( strpos( $member_chiya["name"], "千夜") !== false ){
-    $member_chiya["is_hopping"] = true;
-    $hopping_count++;
-}else{
-    $member_chiya["is_hopping"] = false;
-}
-
-if( strpos( $member_syaro["name"], "紗路") !== false or strpos( $member_syaro["name"], "シャロ" !== false) ){
-    $member_syaro["is_hopping"] = true;
-    $hopping_count++;
-}else{
-    $member_syaro["is_hopping"] = false;
-}
-
-if( strpos( $member_maya["name"], "麻耶") !== false or strpos( $member_maya["name"], "マヤ" !== false) ){
-    $member_maya["is_hopping"] = true;
-    $hopping_count++;
-}else{
-    $member_maya["is_hopping"] = false;
-}
-
-if( strpos( $member_megu["name"], "恵") !== false or strpos( $member_megu["name"], "メグ" !== false) ){
-    $member_megu["is_hopping"] = true;
-    $hopping_count++;
-}else{
-    $member_megu["is_hopping"] = false;
-}
-
-if( strpos( $member_mocha["name"], "モカ") !== false ){
-    $member_mocha["is_hopping"] = true;
-    $hopping_count++;
-}else{
-    $member_mocha["is_hopping"] = false;
-}
-
 
 // 1人当たりの数
 $percentage_per_member = floor( 100 / $member_count );
 
-// ?の余り これいる？
+// ☝の余り これいる？
 $percentage_per_member_remainder = 100 - $percentage_per_member * $member_count;
 
 $total_percent = $percentage_per_member * $hopping_count;
 
-if( $hopping_count == $member_count ){
+// 全員ごちうさキャラになっているかどうかを確認する
+if( $hopping_count === $member_count ){
     $is_all_hopping = true;
     $total_percent = 100;
 }else{
     $is_all_hopping = false;
 }
-
 
 $result = array(
     "status" => "success",
@@ -138,21 +136,9 @@ $result = array(
         "total_percent" => $total_percent, 
         "percentage_per_member" => $percentage_per_member,
         "percentage_per_member_remainder" => $percentage_per_member_remainder,
-        "members" => array(
-            "count" => $member_count,
-            "cocoa" => $member_cocoa,
-            "chino" => $member_chino,
-            "rize" => $member_rize,
-            "chiya" => $member_chiya,
-            "syaro" => $member_syaro,
-            "maya" => $member_maya,
-            "megu" => $member_megu,
-            "mocha" => $member_mocha,
-            "hopping_count" => $hopping_count,
-            "is_all_hopping" => $is_all_hopping
-        )
+        "members" => $member_info_list,
     ),
-    "message" => null
+    "message" => null,
 );
 
 echo json_encode( $result , JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES ) ;
--- user_information.php
+++ user_information.php
@@ -9,30 +9,45 @@ function h( $str ) {
 
 if( isset( $_GET["id"] ) && is_numeric( $_GET["id"] ) ) {
     $id = h( $_GET["id"] );
-    $html = file_get_contents('https://twitter.com/intent/user?user_id='.$id);
-
-    $pattern_name = '#<title>(.*?) \(@[a-z0-9_]{1,15}\) さんはTwitterを使ってます</title>#i';
-    $pattern_profile_image_url_https = '<img class="photo" src="(.+?)".+?>';
-    $pattern_screen_name = '#<span class="nickname">@([a-z0-9_]{1,15})</span>#';
-    // name
-    preg_match( $pattern_name, $html , $name );
     
-    // screen_name
-    preg_match( $pattern_screen_name, $html , $screen_name );
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://twitter.com/intent/user?user_id='.$id);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);  // タイムアウト秒数を指定
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,false);
+    $html = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
     
-    // profile_image_url, profile_image_url_https
-    preg_match( $pattern_profile_image_url_https, $html , $profile_image_url_https );
-    $profile_image_url_https = str_replace( "200x200", "normal", $profile_image_url_https );
-    $profile_image_url = str_replace( "https", "http", $profile_image_url_https );
-
-    $result = array(
-        "is_exist" => true,
-        "id" => $id,
-        "screen_name" => $screen_name[1],
-        "name" => $name[1],
-        "profile_image_url" => $profile_image_url[1],
-        "profile_image_url_https" => $profile_image_url_https[1],
-    );
+    if($httpCode === 200){
+        $pattern_name = '#<title>(.*?) \(@[a-z0-9_]{1,15}\) さんはTwitterを使ってます</title>#i';
+        $pattern_profile_image_url_https = '<img class="photo" src="(.+?)".+?>';
+        $pattern_screen_name = '#<span class="nickname">@([a-z0-9_]{1,15})</span>#';
+        // name
+        preg_match( $pattern_name, $html , $name );
+        
+        // screen_name
+        preg_match( $pattern_screen_name, $html , $screen_name );
+        
+        // profile_image_url, profile_image_url_https
+        preg_match( $pattern_profile_image_url_https, $html , $profile_image_url_https );
+        $profile_image_url_https = str_replace( "200x200", "normal", $profile_image_url_https );
+        $profile_image_url = str_replace( "https", "http", $profile_image_url_https );
+        
+        $result = array(
+            "is_exist" => true,
+            "id" => $id,
+            "screen_name" => $screen_name[1],
+            "name" => $name[1],
+            "profile_image_url" => $profile_image_url[1],
+            "profile_image_url_https" => $profile_image_url_https[1],
+        );
+    }
+    else {
+        $result = array(
+            "message" => "HTMLの取得に失敗しました。(ステータスコード:".$httpCode.")",
+        );
+    }
 } else {
     $result = array(
         "message" => "不正なidです。",
